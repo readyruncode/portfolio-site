350c88b3e29d3e0f3786019aefb7bd48f2f94928
Henrik Forsberg <henrik.forsberg@teknomedia.se>
2019-08-13 21:59:13 +0200

    Remove redundant router module and menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { AboutComponent } from './components/pages/about/about.component';
@@ -10,15 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
 import { PortfolioComponent } from './components/pages/portfolio/portfolio.component';
-import { MenuComponent } from './components/menu/menu.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
-
-// const appRoutes: Routes = [
-// 	{ path: '', component: HomeComponent },
-// 	{ path: 'about', component: AboutComponent },
-// 	{ path: 'bio', component: BioComponent },
-// 	{ path: 'projects', component: ProjectsComponent },
-// ];
 
 @NgModule({
 	declarations: [
@@ -29,15 +20,10 @@ import { ContactComponent } from './components/pages/contact/contact.component';
 		HeaderComponent,
 		HomeComponent,
 		ProjectCardComponent,
-		PortfolioComponent,
-		MenuComponent
+		PortfolioComponent
 	],
 	imports: [
 		BrowserModule,
-		// RouterModule.forRoot(
-		// 	appRoutes,
-		// 	{ enableTracing: true } // Debugging purposes only
-		// ),
 		FormsModule
 	],
 	providers: [],
